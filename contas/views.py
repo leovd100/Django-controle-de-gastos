fa71c1b36050bd17308b8e60564ed38f0bd389ce
Leonardo <demetrio.vd@hotmail.com>
2020-06-03 19:24:50 -0300

    modificacao do codigo html

--- contas/views.py
+++ contas/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
-
+from datetime import datetime
 
 # Create your views here.
 def home(requests):
-	data = {}
-	data['agora'] = datetime.datetime.now()
+	data = {
+		'transasoes':['t1','t2','t3']
+	}
+	
+	#data_today = f'{datetime.now().day}/{datetime.now().month}/{datetime.now().year}'
+
+	data['agora'] = datetime.now().strftime('%d/%m/%Y')
+
+
+
 	#html = '<html><body>Data e Hora atual %s</body></html>'% agora
 	return render(requests, 'contas/home.html',data)
 
